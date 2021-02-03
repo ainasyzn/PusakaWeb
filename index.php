9afbf9f5e9a16a790714238f05e9e0e25538f210
ainasyzn <47598986+ainasyzn@users.noreply.github.com>
2021-02-03 15:12:35 +0800

    init

---
+++ index.php
@@ -0,0 +1,79 @@
+<!-- Add your content of head and header -->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta content="IE=edge" http-equiv="X-UA-Compatible">
+  <meta content="width=device-width,initial-scale=1" name="viewport">
+  <meta content="description" name="description">
+  <meta name="google" content="notranslate" />
+  <meta content="Mashup templates have been developped by Orson.io team" name="author">
+
+  <!-- Disable tap highlight on IE -->
+  <meta name="msapplication-tap-highlight" content="no">
+  
+  <link href="./assets/apple-touch-icon.png" rel="apple-touch-icon">
+  <link href="./assets/favicon.ico" rel="icon">
+
+    <title>Pusaka Pelangi Sdn Bhd</title>
+
+    <!-- Bootstrap CSS -->
+    <link rel="shortcut icon" type="image/icon" href="v1/img/logo2.png"/>
+<link href="./main.97292821.css" rel="stylesheet"></head>
+
+<body>
+
+ <!-- Add your content of header -->
+<header>
+
+</header>
+
+
+
+
+
+
+
+
+<div class="">
+    <div class='container-fluid'>
+        <div class="row map-container">
+            <div id="map"></div>
+            <div class="col-xs-10 col-md-4 contact-block-container reveal">
+                <div class="row">
+                    <div class="col-xs-12 col-sm-6">
+                        <h3><img class="img-fulid" src="assets/images/homelogo2.png" alt="" style="min-width:100px;max-width:200%"></h3>
+                        <br>
+                        <a href="v1/index.php" class="btn btn-danger">Laman Utama</a>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+<script>
+document.addEventListener("DOMContentLoaded", function (event) {
+    
+  googleMapInit(); 
+  scrollToAnchor();
+  scrollRevelation('reveal');
+});
+</script>
+
+
+
+<script>
+document.addEventListener("DOMContentLoaded", function (event) {
+  navbarToggleSidebar();
+});
+</script>
+
+
+<script type="text/javascript" src="./main.faaf51f9.js"></script>
